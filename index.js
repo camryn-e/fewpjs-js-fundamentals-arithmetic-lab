e04b1d5d0e4c46d039d92fd22022ef44993cc956
camryn williams <williams.camryn.e@gmail.com>
2021-03-17 15:42:39 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let beep = (Math.random() * 10);
+
+let random = Math.round(beep);
+
+let num3 = 29;
+
+let num4 = 5;
+
+let mod = num3%num4;
+
+let max = Math.max(2, 15, 20);
